9d1e4d70214c2f327d6ccc1a120820dc4f650008
wangkeming <1766140586@qq.com>
2018-05-02 17:21:20 +0800

    减少查询返回的无用字段

--- src/main/java/com/abc/newsserversec/common/StaticVariable.java
+++ src/main/java/com/abc/newsserversec/common/StaticVariable.java
@@ -9,7 +9,7 @@ public class StaticVariable {
     public static String searchProductIncludeFields = "\"id\",\"product_name_ch\",\"maker_name_ch\",\"agent\",\"src_loc\",\"main_class\",\"class_code\",\"product_state\",\"product_mode\"";
 
     public static String searchProductExcludeFields = "\"file_id\",\"type\",\"record_number\",\"data_source_code\",\"manage_type\",\"record_main_type\",\"record_sub_type\"," +
-            "\"keep_field_first\",\"keep_field_second\",\"keep_field_third\",\"diseaseFlags\",\"deptFlags\",\"import_date\"";
+            "\"keep_field_first\",\"keep_field_second\",\"keep_field_third\",\"diseaseFlags\",\"deptFlags\",\"import_date\",\"path\",\"host\",\"@version\",\"@timestamp\"";
 
     public static String productAggsProductName = "{ \"tags\":{ \"terms\":{ \"field\":\"product_name_agg\" }, \"aggs\":{ \"company_name\":{ \"terms\":{ \"field\":\"company_name_agg\" } }," +
             "\"class_code\":{ \"terms\":{ \"field\":\"class_code\" } }, \"max_date\":{ \"max\":{ \"field\":\"approval_date\" } }, \"min_date\":{ \"min\":{ \"field\":\"approval_date\" } } } } }";
