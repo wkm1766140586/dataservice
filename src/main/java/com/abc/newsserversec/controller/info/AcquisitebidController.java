ce0e338259763278730e03b5c484bb66f9932c18
wangkeming <1766140586@qq.com>
2018-06-07 17:00:35 +0800

    修改

--- src/main/java/com/abc/newsserversec/controller/info/AcquisitebidController.java
+++ src/main/java/com/abc/newsserversec/controller/info/AcquisitebidController.java
@@ -1,6 +1,7 @@
 package com.abc.newsserversec.controller.info;
 
 import com.abc.newsserversec.common.StaticVariable;
+import com.abc.newsserversec.model.info.ESCount;
 import com.abc.newsserversec.model.info.ESResultRoot;
 import com.abc.newsserversec.model.info.Hit;
 import com.abc.newsserversec.common.HttpHandler;
@@ -9,8 +10,10 @@ import com.google.gson.GsonBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * 集采中标数据查询控制器
@@ -20,51 +23,88 @@ public class AcquisitebidController {
 
     /**
      * 根据名称来查询集采中标数据
-     * @param keyword
+     * @param
      * @return
      * @throws IOException
      */
-    @RequestMapping("/acquisitebid/search_acquisitebid_name")
-    public String searchAcquisitebidByname(String keyword, HttpServletResponse response) throws IOException {
+    @RequestMapping("/method/search_acquisitebid_name")
+    public String searchAcquisitebidByname(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setHeader("Access-Control-Allow-Origin", "*");
+
+        String num = request.getParameter("num");
+        String keyword = request.getParameter("keyword");
+        if(num == null || keyword == null) return "fail";
+        if(num.equals("") || keyword.equals("")) return "fail";
+
+        String size = request.getParameter("size");
+
         String esRequest = StaticVariable.esRequest;
+        int from = Integer.valueOf(num);
+        if(size != null) { from = from * Integer.valueOf(size); }
+        else{ from = from * 10; }
+
         String condition = "product_name:\\\\\""+keyword+"\\\\\"";
+        esRequest = esRequest.replaceFirst("\"#from\"",String.valueOf(from));
+        if(size == null){ esRequest = esRequest.replaceFirst("\"#size\"","10"); }
+        else{ esRequest = esRequest.replaceFirst("\"#size\"",size); }
+        esRequest = esRequest.replaceFirst("#includes","*");
+        esRequest = esRequest.replaceFirst("\"#excludes\"","");
         String postbody = esRequest.replaceFirst("#query",condition);
+        postbody = postbody.replaceFirst("\"#aggs\"","{}");
         System.out.println("postbody="+postbody);
-        String ret = HttpHandler.httpPostCall("http://localhost:9200/bid/_search", postbody);
-        System.out.println("es return:"+ret);
+
+        String ret = HttpHandler.httpPostCall("http://localhost:9200/acquisitebid/_search", postbody);
         ESResultRoot retObj = new GsonBuilder().create().fromJson(ret, ESResultRoot.class);
-        System.out.println("retObj:"+retObj.hits.hits.size());
         SourceSet productSet = new SourceSet();
         for(Hit hit:retObj.hits.hits){
-            productSet.add(hit._source);
+            Map map = (Map)hit._source;
+            map.put("_id",hit._id);
+            productSet.add(map);
         }
-        productSet.setMatchCount(productSet.getDatas().size());
+        if(from == 0) {
+            //计数
+            String esCount = StaticVariable.esCount;
+            esCount = esCount.replaceFirst("#query", condition);
+            String countRet = HttpHandler.httpPostCall("http://localhost:9200/acquisitebid/_count", esCount);
+            ESCount esCt = new GsonBuilder().create().fromJson(countRet, ESCount.class);
+            productSet.setMatchCount(esCt.count);
+        }
+        System.out.println(new GsonBuilder().create().toJson(productSet));
         return new GsonBuilder().create().toJson(productSet);
     }
 
     /**
      * 根据id查询集采中标数据
-     * @param id
+     * @param
      * @return
      * @throws IOException
      */
-    @RequestMapping("/acquisitebid/search_acquisitebid_id")
-    public String searchAcquisitebidById(String id, HttpServletResponse response) throws IOException {
+    @RequestMapping("/method/search_acquisitebid_id")
+    public String searchAcquisitebidById(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setHeader("Access-Control-Allow-Origin", "*");
+
+        String id = request.getParameter("id");
+        if(id == null) return "fail";
+        if(id.equals("")) return "fail";
+
         String esRequest = StaticVariable.esRequest;
         String condition = "id:\\\\\""+id+"\\\\\"";
+        esRequest = esRequest.replaceFirst("\"#from\"",String.valueOf(0));
+        esRequest = esRequest.replaceFirst("\"#size\"","10");
+        esRequest = esRequest.replaceFirst("\"#includes\"","");
+        esRequest = esRequest.replaceFirst("\"#excludes\"",StaticVariable.ExcludeFields);
         String postbody = esRequest.replaceFirst("#query",condition);
+        postbody = postbody.replaceFirst("\"#aggs\"","{}");
         System.out.println("postbody="+postbody);
-        String ret = HttpHandler.httpPostCall("http://localhost:9200/bid/_search", postbody);
-        System.out.println("es return:"+ret);
+
+        String ret = HttpHandler.httpPostCall("http://localhost:9200/acquisitebid/_search", postbody);
         ESResultRoot retObj = new GsonBuilder().create().fromJson(ret, ESResultRoot.class);
-        System.out.println("retObj:"+retObj.hits.hits.size());
         SourceSet productSet = new SourceSet();
         for(Hit hit:retObj.hits.hits){
-            productSet.add(hit._source);
+            Map map = (Map)hit._source;
+            map.put("_id",hit._id);
+            productSet.add(map);
         }
-        productSet.setMatchCount(productSet.getDatas().size());
         return new GsonBuilder().create().toJson(productSet);
     }
 
